459e303e57199a378f861599d681dea0653dea81
sebasruiz09 <sebasruiz909@gmail.com>
2023-03-09 23:11:04 -0500

    FIX : import entities

--- src/databases/database.service.ts
+++ src/databases/database.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@nestjs/common';
 import { TypeOrmOptionsFactory, TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { ConfigService } from '@nestjs/config';
+import { Rating } from 'src/books/entities';
+import { Book } from '../books/entities/book.entity';
+import { User } from 'src/auth/entities/users.entity';
 
 @Injectable()
 export class DatabaseService implements TypeOrmOptionsFactory {
@@ -14,7 +17,7 @@ export class DatabaseService implements TypeOrmOptionsFactory {
       username: this.configService.get<string>('DBUSER'),
       password: this.configService.get<string>('DBPASSWORD'),
       database: this.configService.get<string>('DBNAME'),
-      entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+      entities: [User, Rating, Book],
       synchronize: true,
       autoLoadEntities: true,
     };
